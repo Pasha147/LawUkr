f570f92003ba921bb438625c062e13e878c6df2e
Pasha <75729255+Pasha147@users.noreply.github.com>
2022-01-20 15:28:42 +0200

    Validation with regular expressions

--- Lawukr/src/app/consultation/consultation.component.ts
+++ Lawukr/src/app/consultation/consultation.component.ts
@@ -26,10 +26,9 @@ export class ConsultationComponent implements OnInit {
 
   // onSubmit(obj:any, inp:any): void {
   onSubmit(): void {
-  //  console.log(obj);
-  //  console.log(obj.form.invalid);
-  //  console.log(inp.valid);
-   
+    //  console.log(obj);
+    //  console.log(obj.form.invalid);
+    //  console.log(inp.valid);
 
     // this.getPost.postForm(this.formData).subscribe((a) => {
     //   console.log('a>', a);
@@ -41,5 +40,21 @@ export class ConsultationComponent implements OnInit {
     //     text: '',
     //   };
     // });
+    let reg = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
+    if (reg.test(this.formData.phone)) {
+      console.log('Phone:Yes');
+    } else {
+      console.log('Phone:No');
+    }
+let regDate=/^([1-9][0-9]?)\\.([1-9][0-9]?)\\.([1-2][0-9]{3})$/;
+let regDate1=/^([0-9]{2})\\.([0-9]{2})\\.([1-2][0-9]{3})$/;
+let regex = new RegExp("^([0-9]{2})\\.([0-9]{2})\\.([1-2][0-9]{3})$");
+
+if(regex.test(this.formData.bearthday))
+{
+      console.log('Bearthday:Yes');
+    } else {
+      console.log('Bearthday:No');
+    }
   }
 }
