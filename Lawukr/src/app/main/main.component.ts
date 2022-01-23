457a1a117a781ae073b422224de7430ca2ee18b1
Pasha <75729255+Pasha147@users.noreply.github.com>
2022-01-23 13:20:59 +0200

    The messages on the main page have been created

--- Lawukr/src/app/main/main.component.ts
+++ Lawukr/src/app/main/main.component.ts
@@ -1,16 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  constructor(private messageServ: MessageService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  imgUrl = './assets/ov.png';
+
+  // messagesBarPhone = this.items[0].show;
+  // messagesBarEmail = this.items[1].show;
+  messagesBarPhone = false;
+  messagesBarEmail = false;
+  items = [
+    {
+      name: 'phone',
+      message: 'Номер скопійовано',
+      strClip: '0687018511',
+      show: false,
+    },
+    {
+      name: 'email',
+      message: 'Адресу скопійовано',
+      strClip: 'oksanazay@ukr.net',
+      show: false,
+    },
+  ];
+
+  contactClick(str: string) {
+    if (this.items.find((i) => i.show)) {
+      return;
+    }
+    let item = this.items.find((i) => i.name === str);
+    this.messageServ.add(item!.message);
+    item!.show = true;
+    navigator.clipboard.writeText(item!.strClip);
+    setTimeout(() => {
+      this.messageServ.clear();
+      item!.show = false;
+    }, 2000);
   }
- imgUrl = './assets/ov.png';
-
 }
